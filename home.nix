4c8ff96078e502baaecd5f51c88ddd5874521535
Abhishek Dasgupta <abhidg@trenozoic.net>
2022-03-21 21:11:38 +0000

    xresources: move out of main config

--- home.nix
+++ home.nix
@@ -75,11 +75,7 @@
     };
   };
   
-  xresources.properties = {
-    "Xft.dpi" = 128;
-    "URxvt.font" = "xft:monospace:size=12,xft:noto emoji:size=12";
-    "URxvt.boldFont" = "xft:monospace:weight=bold:size=12,xft:noto emoji:weight=bold:size=12,xft:symbola";
-  };
+  xresources.properties = import ./xresources.nix;
 
   programs.bash = {
     enable = true;
---
+++ xresources.nix
@@ -0,0 +1,5 @@
+{
+  "Xft.dpi" = 224;
+  "URxvt.font" = "xft:monospace:size=12,xft:noto emoji:size=12";
+  "URxvt.boldFont" = "xft:monospace:weight=bold:size=12,xft:noto emoji:weight=bold:size=12,xft:symbola";
+}
