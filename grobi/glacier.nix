4ac755d32911853993d0578f05a46d621d019989
Abhishek Dasgupta <abhidg@trenozoic.net>
2022-04-21 16:31:44 +0100

    grobi: add work monitor

--- grobi/glacier.nix
+++ grobi/glacier.nix
@@ -15,6 +15,17 @@
       ];
     }
     {
+      name = "Work";
+      outputs_connected = [ "DP-3" ];
+      configure_single = "DP-3";
+      primary = true;
+      atomic = true;
+      execute_after = [
+        "${pkgs.xorg.xrandr}/bin/xrandr --dpi 224"
+        "${pkgs.i3}/bin/i3-msg restart"
+      ];
+    }
+    {
       name = "Mobile";
       outputs_disconnected = [ "DP-1" ];
       configure_single = "eDP-1";
